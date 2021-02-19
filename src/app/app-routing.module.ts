ccc073a507028c666808fc0701e17a47cd5db389
cio@mowin-tech.com <cio@mowin-tech.com>
2021-02-18 23:13:39 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeesComponent } from './pages/employees/employees.component';
+import { RegisterEmployeeComponent } from './pages/register-employee/register-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'employees', pathMatch: 'full' },
+    { path: 'employees', component: EmployeesComponent },
+    { path: 'register-employee', component: RegisterEmployeeComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './pages/employees/employees.component';
+import { SharedModule } from './shared/shared.module';
+import { RegisterEmployeeComponent } from './pages/register-employee/register-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    RegisterEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
